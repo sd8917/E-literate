f45a5706946a495fa251108caf38eb0365818118
sudhanshu <sudhanshuraj8917@gmail.com>
2020-09-03 00:39:32 -0700

    hard coded queries

--- eliterate/userprofile/views.py
+++ eliterate/userprofile/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import UserDetail
+from django.db.models import Q
 
 def userpage(request):
     getuser = UserDetail.objects.all() #contain all detail about the user
     print(getuser)
     return render(request, 'home.html',{'getuser': getuser})
 
+#this function return the user query into templates
+#working on
 def search(request):
-    pass
+    if request.GET:
+        search_term = request.GET['search_term']
+    
+        search_results = UserDetail.objects.filter(
+           Q(certificatenumber__iexact = search_term) 
+        )
+        print(str(search_term))
+        context = {
+            'search_term' : search_term,
+            'search_results': search_results.filter(manager=request.user)
+        }
+        return render(request, 'home.html', context)
+        
+    else:
+
+        return redirect(request, 'home.html')
